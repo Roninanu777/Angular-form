935bb27450d0a8441297698173db240a381f3744
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-06-25 13:20:49 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-form';
+
+  constructor(private fb: FormBuilder){}
+  
+  userprofileForm = this.fb.group({
+    firstName: ['',Validators.required],
+    lastName: ['', Validators.required],
+    userName: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required,Validators.minLength(8)]],
+    dob: ['', Validators.required],
+    country: ['', Validators.required]
+  });
+
+  get userName(){
+    return this.userprofileForm.get('userName');
+  }
+
+  get password(){
+    return this.userprofileForm.get('password');
+  }
+
+  get firstName(){
+    return this.userprofileForm.get('firstName');
+  }
+
+  get lastName(){
+    return this.userprofileForm.get('lastName');
+  }
+
+  get dob(){
+    return this.userprofileForm.get('dob');
+  }
+
+  get country(){
+    return this.userprofileForm.get('country');
+  }
+
+  onSubmit(){
+    console.warn(this.userprofileForm.value);
+  }
+
 }
